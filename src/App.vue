54696107dbed46a0b623372bbb63dcf3d1d7558d
scokljat <selma.cokljat@hotmail.com>
2022-03-04 02:18:34 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template><SideBar /></template>
+<script>
+import SideBar from "./components/SideBar.vue";
+export default {
+  name: "App",
+  components: {
+    SideBar,
+  },
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <i class="fas fa-envelope"></i>
+</template>
+
+<script>
+export default {
+  name: "SideBar",
+};
+</script>
+<style>
+.fas {
+  font-size: 50px;
+}
+</style>
