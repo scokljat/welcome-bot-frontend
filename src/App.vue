2e5361cfb68ad4b08d790cbb9b7d0491cf17e72f
scokljat <selma.cokljat@hotmail.com>
2022-03-08 03:49:31 -0500

    Update sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,38 @@
-<script setup>
-import { RouterView } from 'vue-router';
-</script>
-
 <template>
-  <TheSidebar />
-  <div :style="{ 'margin-left': sidebarWidth }">
+  <div id="app">
+    <TheSidebar />
     <router-view />
   </div>
 </template>
 
 <script>
-import TheSidebar from './components/sidebar/TheSidebar.vue';
-import { sidebarWidth } from './components/sidebar/state.js';
+import TheSidebar from './components/TheSidebar.vue';
+
 export default {
   components: { TheSidebar },
-  setup() {
-    return { sidebarWidth };
-  },
 };
 </script>
 
 <style lang="scss">
 @import './style/main';
+
+.container-center {
+  background-color: var(--background);
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.card {
+  padding: 2rem 4rem;
+  width: $icon-w;
+  height: 200px;
+  width: 300px;
+  text-align: center;
+  border: 1px solid var(--accent-color);
+  border-radius: 4px;
+  background-color: var(--white);
+}
 </style>
---
+++ src/components/TheSidebar.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="sidebar" :style="{ width: width }">
+    <span
+      class="collapse-icon"
+      :class="{ 'rotate-180': collapsed }"
+      @click="toggleSidebar"
+    >
+      <i class="fas fa-angle-double-left" />
+    </span>
+    <div class="sidebarlink">
+      <router-link to="/messages"
+        ><i class="fas fa-envelope"></i
+        ><span class="link_text">Messages</span></router-link
+      >
+      <router-link to="/schedules"
+        ><i class="fas fa-calendar-check"></i
+        ><span>Schedules</span></router-link
+      >
+      <router-link to="/triggers"
+        ><i class="fas fa-comment-alt"></i><span>Triggers</span></router-link
+      >
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TheSidebar',
+  data: () => {
+    return {
+      collapsed: false,
+      sidebarWidth: 100,
+      sidebarWidthCollapsed: 55,
+    };
+  },
+  computed: {
+    width() {
+      return `${
+        this.collapsed ? this.sidebarWidthCollapsed : this.sidebarWidth
+      }px`;
+    },
+  },
+  methods: {
+    toggleSidebar() {
+      return (this.collapsed = !this.collapsed);
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+.sidebar {
+  background-color: var(--blue);
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  transition: 0.3s ease;
+  display: flex;
+  flex-direction: column;
+}
+.sidebarlink {
+  display: flex;
+  flex-direction: column;
+  margin-top: 40px;
+}
+.link {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+  padding: 10px;
+  margin: 20px;
+  color: var(--white-60);
+  text-decoration: none;
+}
+.router-link-active > i,
+.router-link-active > .link_text {
+  color: var(--white-100) !important;
+}
+.collapse-icon {
+  position: absolute;
+  padding: 10px;
+  color: var(--white-100);
+  transition: 0.2s linear;
+  margin-bottom: 20px;
+}
+.rotate-180 {
+  transform: rotate(-180deg);
+  transition: 0.2s linear;
+}
+@include tablet-md {
+  .collapse-icon {
+    display: none;
+  }
+  .rotate-180 {
+    transform: none;
+    transition: none;
+  }
+}
+</style>
