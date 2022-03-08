960ac81263e05ce8760b87b22680175fc3d2484f
thedev966 <salkobalic3@gmail.com>
2022-03-08 02:43:30 -0500

    Update App.vue, import components

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
-<template><SideBar /></template>
+<template>
+  <SideBar />
+  <TheFloatingButton />
+  <AppModal modal-title="Create Trigger">
+    <ModalCreateTrigger />
+  </AppModal>
+</template>
 <script>
 import SideBar from './components/SideBar.vue';
+import AppModal from './components/AppModal.vue';
+import ModalCreateTrigger from './components/ModalCreateTrigger.vue';
+import TheFloatingButton from './components/TheFloatingButton.vue';
 export default {
   name: 'App',
   components: {
     SideBar,
+    AppModal,
+    ModalCreateTrigger,
+    TheFloatingButton,
   },
 };
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@import './style/main';
+</style>
