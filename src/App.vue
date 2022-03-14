0da38425fb50ee760333bff55cc7a955fc62cc65
thedev966 <salkobalic3@gmail.com>
2022-03-14 05:09:27 -0400

    Add data table for messages

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
   <AppModal modal-title="Create Schedule">
     <ModalCreateSchedule />
   </AppModal>
+  <MessagesDataTable />
 </template>
 
 <script>
 import AppModal from './components/AppModal.vue';
 import TheFloatingButton from './components/TheFloatingButton.vue';
 import ModalCreateSchedule from './components/ModalCreateSchedule.vue';
+import MessagesDataTable from './components/MessagesDataTable.vue';
 export default {
   name: 'App',
   components: {
     AppModal,
     TheFloatingButton,
     ModalCreateSchedule,
+    MessagesDataTable,
   },
 };
 </script>
---
+++ src/components/MessagesDataTable.vue
@@ -0,0 +1,53 @@
+<template>
+  <el-table :data="tableData">
+    <el-table-column
+      v-for="col in tableLabel"
+      :key="col.id"
+      :prop="col.prop"
+      :label="col.label"
+      :sortable="col.isSortable"
+      :column-key="col.prop"
+      :width="col.width"
+    />
+    <el-table-column prop="action" label="">
+      <i class="fa-solid fa-clipboard-check icon"></i>
+      <i class="fa-solid fa-bell icon"></i>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: 'MessagesDataTable',
+  data() {
+    return {
+      tableData: [
+        {
+          title: 'Message 1',
+          text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+          date: '2016-04-03',
+        },
+      ],
+      tableLabel: [
+        {
+          id: 1,
+          label: 'Title',
+          prop: 'title',
+          isSortable: true,
+          width: '170',
+        },
+        { id: 2, label: 'Text', prop: 'text', isSortable: true, width: '320' },
+        {
+          id: 3,
+          label: 'Created At',
+          prop: 'date',
+          isSortable: true,
+          width: '135',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
