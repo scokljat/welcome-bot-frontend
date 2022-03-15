849fee4df6bbc89630e4af8b503789ff237fee2b
thedev966 <salkobalic3@gmail.com>
2022-03-15 07:22:56 -0400

    Change element plus import and style from global to local

--- src/components/SchedulesDataTable.vue
+++ src/components/SchedulesDataTable.vue
@@ -23,8 +23,16 @@
 </template>
 
 <script>
+import { ElTable, ElTableColumn, ElPagination } from 'element-plus';
+import 'element-plus/dist/index.css';
+
 export default {
   name: 'SchedulesDataTable',
+  components: {
+    ElTable,
+    ElTableColumn,
+    ElPagination,
+  },
   data() {
     return {
       tableData: [
