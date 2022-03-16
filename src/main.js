6aa7d249cebb9900f0006c0f06da359cb44bf609
thedev966 <salkobalic3@gmail.com>
2022-03-16 07:11:32 -0400

    Update style import for data table components

--- src/main.js
+++ src/main.js
@@ -3,4 +3,9 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// table imports
+import 'element-plus/es/components/table/style/css';
+import 'element-plus/es/components/table-column/style/css';
+import 'element-plus/es/components/pagination/style/css';
+
 createApp(App).use(store).use(router).mount('#app');
