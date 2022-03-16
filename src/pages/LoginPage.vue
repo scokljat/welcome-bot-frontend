16241b30695059546f946bb8bc7014c5fb436efb
scokljat <selma.cokljat@hotmail.com>
2022-03-16 08:32:36 -0400

    Add login page with style

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,3 +1,81 @@
 <template>
-  <h1>Log in Page</h1>
+  <div class="container">
+    <h1>Welcome Bot</h1>
+    <h2>Log in</h2>
+    <button @click="handleClickSignIn">
+      <i class="fab fa-google"></i>Sign in
+    </button>
+    <button @click="handleClickSignOut">Sign out</button>
+  </div>
+  <p>{{ Vue3GoogleOauth.isAuthorized }}</p>
 </template>
+<script>
+import { inject } from 'vue';
+export default {
+  name: 'LoginPage',
+  setup() {
+    const Vue3GoogleOauth = inject('Vue3GoogleOauth');
+    console.log(Vue3GoogleOauth.instance);
+    return {
+      Vue3GoogleOauth,
+    };
+  },
+  // data() {
+  //   return { isLoggedIn: false };
+  // },
+  methods: {
+    async handleClickSignIn() {
+      try {
+        const googleUser = await this.$gAuth.signIn();
+        // this.isLoggedIn = this.$gAuth.isAuthorized;
+        // console.log(
+        //   'getAuthResponse$G',
+        //   this.$gAuth.GoogleAuth.get().getAuthResponse()
+        // );
+        //this.isLoggedIn = this.$gAuth.isAuthorized;
+        if (!googleUser) {
+          return null;
+        }
+        console.log('googleUser', googleUser);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async handleClickSignOut() {
+      try {
+        await this.$gAuth.signOut();
+        // Vue3GoogleOauth.getAuthInstance().disconnect();
+      } catch (error) {
+        console.error(error);
+      }
+    },
+  },
+};
+</script>
+<style scoped lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 80vh;
+  h1 {
+    font-size: 70px;
+  }
+  h2 {
+    font-size: 30px;
+  }
+}
+button {
+  background-color: var(--primary);
+  width: 150px;
+  height: 70px;
+  font-size: $text-lg;
+  margin: 20px;
+  border-radius: 4px;
+  i {
+    font-size: 1.5rem;
+    padding-right: 10px;
+  }
+}
+</style>
