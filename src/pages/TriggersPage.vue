99bd413d46430204bf1fe30b6226b84cf0bad7d8
thedev966 <salkobalic3@gmail.com>
2022-03-17 09:27:46 -0400

    Add triggers page

--- src/pages/TriggersPage.vue
+++ src/pages/TriggersPage.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>Triggers Page</h1>
+  <div class="triggers-page">
+    <TheFloatingButton />
+    <AppModal modalTitle="Create Trigger">
+      <ModalCreateTrigger />
+    </AppModal>
+    <TriggersDataTable />
+  </div>
 </template>
+
+<script>
+import TheFloatingButton from '@/components/TheFloatingButton.vue';
+import AppModal from '@/components/AppModal.vue';
+import ModalCreateTrigger from '@/components/ModalCreateTrigger.vue';
+import TriggersDataTable from '@/components/TriggersDataTable.vue';
+
+export default {
+  name: 'MessagesPage',
+  components: {
+    TheFloatingButton,
+    AppModal,
+    ModalCreateTrigger,
+    TriggersDataTable,
+  },
+};
+</script>
+
+<style scoped>
+.messages-page {
+  border: 1px solid var(--border-color-1);
+}
+</style>
