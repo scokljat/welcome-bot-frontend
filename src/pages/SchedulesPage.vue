e432bb9cc64672276a528a1ccf6f5d093bf94c54
thedev966 <salkobalic3@gmail.com>
2022-03-17 09:07:29 -0400

    Add schedules page

--- src/pages/SchedulesPage.vue
+++ src/pages/SchedulesPage.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>Schedules Page</h1>
+  <div class="schedules-page">
+    <TheFloatingButton />
+    <AppModal modalTitle="Create Schedule">
+      <ModalCreateSchedule />
+    </AppModal>
+    <SchedulesDataTable />
+  </div>
 </template>
+
+<script>
+import TheFloatingButton from '@/components/TheFloatingButton.vue';
+import AppModal from '@/components/AppModal.vue';
+import ModalCreateSchedule from '@/components/ModalCreateSchedule.vue';
+import SchedulesDataTable from '@/components/SchedulesDataTable.vue';
+
+export default {
+  name: 'MessagesPage',
+  components: {
+    TheFloatingButton,
+    AppModal,
+    ModalCreateSchedule,
+    SchedulesDataTable,
+  },
+};
+</script>
+
+<style scoped>
+.schedules-page {
+  border: 1px solid var(--border-color-1);
+}
+</style>
