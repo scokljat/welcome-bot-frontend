9054760054a89cb538165bc30c61b54886559502
thedev966 <salkobalic3@gmail.com>
2022-03-18 11:22:03 +0100

    Fix import style for components in the messages page

--- src/pages/MessagesPage.vue
+++ src/pages/MessagesPage.vue
@@ -9,10 +9,10 @@
 </template>
 
 <script>
-import TheFloatingButton from '../components/TheFloatingButton.vue';
-import AppModal from '../components/AppModal.vue';
-import ModalCreateMessage from '../components/ModalCreateMessage.vue';
-import MessagesDataTable from '../components/MessagesDataTable.vue';
+import TheFloatingButton from '@/components/TheFloatingButton.vue';
+import AppModal from '@/components/AppModal.vue';
+import ModalCreateMessage from '@/components/ModalCreateMessage.vue';
+import MessagesDataTable from '@/components/MessagesDataTable.vue';
 
 export default {
   name: 'MessagesPage',
