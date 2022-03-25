b01e5c23c864997f0e50951025c37f1b623048d7
thedev966 <salkobalic3@gmail.com>
2022-03-25 11:04:10 +0100

    Update message service functions with try/catch block

--- src/api/services/MessagesService.js
+++ src/api/services/MessagesService.js
@@ -2,18 +2,30 @@ import axios from '../http';
 
 const MessagesService = {
   fetchMessages: async ({ pageNumber }) => {
-    const response = await axios.get(
-      `/messages?offset=${pageNumber - 1}&pagesize=15`
-    );
-    return response.data;
+    try {
+      const response = await axios.get(
+        `/messages?offset=${pageNumber - 1}&pagesize=15`
+      );
+      return response.data;
+    } catch (e) {
+      console.log(e.message);
+    }
   },
   fetchAllMessages: async () => {
-    const response = await axios.get('/messages');
-    return response.data;
+    try {
+      const response = await axios.get('/messages');
+      return response.data;
+    } catch (e) {
+      console.log(e.message);
+    }
   },
   deleteMessage: async (id) => {
-    const response = await axios.delete(`/messages/${id}`);
-    return response.data;
+    try {
+      const response = await axios.delete(`/messages/${id}`);
+      return response.data;
+    } catch (e) {
+      console.log(e.message);
+    }
   },
 };
 export default MessagesService;
