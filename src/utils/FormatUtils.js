c914c9ee1a2b0c8b64069ca1d2b4f3f697985b19
scokljat <selma.cokljat@hotmail.com>
2022-03-29 09:32:50 -0400

    Remove formatActive function

--- src/utils/FormatUtils.js
+++ src/utils/FormatUtils.js
@@ -3,17 +3,5 @@ const FormatUtils = {
   formatDate(date, typeFormat) {
     return format(new Date(date), typeFormat);
   },
-  formatActive(isActive) {
-    switch (isActive) {
-      case true:
-        return 'Active';
-      case false:
-        return 'Inactive';
-      case 'Active':
-        return true;
-      case 'Inactive':
-        return false;
-    }
-  },
 };
 export default FormatUtils;
