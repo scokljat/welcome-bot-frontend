ee339017582b357b0525a584db465a77e0f13cc5
scokljat <selma.cokljat@hotmail.com>
2022-04-01 09:07:16 -0400

    Add edittrigger to triggers service

--- src/api/services/TriggersService.js
+++ src/api/services/TriggersService.js
@@ -2,27 +2,40 @@ import axios from '../http';
 
 const TriggersService = {
   fetchTriggers: async (pageNumber) => {
+    const url = `/triggers?offset=${pageNumber - 1}&pagesize=15`;
     try {
-      const url = `/triggers?offset=${pageNumber - 1}&pagesize=15`;
       const response = await axios.get(url);
+
       return response.data;
     } catch (error) {
       console.log(error);
     }
   },
   deleteTrigger: async (id) => {
+    const url = `/triggers/${id}`;
     try {
-      const url = `/triggers/${id}`;
       const response = await axios.delete(url);
+
       return response.data;
     } catch (error) {
       console.log(error);
     }
   },
   createTrigger: async (trigger) => {
+    const url = '/triggers';
     try {
-      const url = '/triggers';
       const response = await axios.post(url, trigger);
+
+      return response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+  editTrigger: async (id, trigger) => {
+    const url = `/triggers/${id}`;
+    try {
+      const response = await axios.put(url, trigger);
+
       return response.data;
     } catch (error) {
       console.log(error);
