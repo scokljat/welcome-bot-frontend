0a3e43a194fae7b2e0f3e60f92945427e2db8083
scokljat <selma.cokljat@hotmail.com>
2022-04-04 06:21:00 -0400

    Add interceptors request

--- src/api/http.js
+++ src/api/http.js
@@ -1,5 +1,22 @@
 import axios from 'axios';
 
-export default axios.create({
+const instance = axios.create({
   baseURL: process.env.VUE_APP_SERVER_URL,
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
